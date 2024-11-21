53d638b7fd12eb14aeeef531def92415c8c68b6c
Patricio Tena <tenapatricio@gmail.com>
2024-11-21 11:57:14 -0600

    added files

---
+++ src/adjectives.js
@@ -0,0 +1,16 @@
+export const adjectives = [
+    "admiring", "adoring", "affectionate", "agitated", "amazing", "angry",
+    "awesome", "blissful", "boring", "brave", "clever", "cool", "compassionate",
+    "competent", "confident", "cranky", "dazzling", "determined", "dreamy",
+    "eager", "ecstatic", "elated", "elegant", "eloquent", "epic", "exciting",
+    "fervent", "focused", "friendly", "frosty", "gallant", "gifted", "goofy",
+    "gracious", "happy", "hardcore", "hopeful", "hungry", "infallible",
+    "inspiring", "interesting", "intelligent", "jolly", "jovial", "keen",
+    "kind", "laughing", "loving", "magical", "mystifying", "nervous", "nice",
+    "nifty", "nostalgic", "objective", "optimistic", "peaceful", "pedantic",
+    "pensive", "practical", "priceless", "quirky", "quizzical", "recursing",
+    "relaxed", "reverent", "romantic", "sad", "serene", "sharp", "silly",
+    "sleepy", "stoic", "stupefied", "suspicious", "sweet", "tender", "thirsty",
+    "trusting", "unruffled", "upbeat", "vibrant", "vigilant", "wizardly",
+    "wonderful", "xenodochial", "youthful", "zealous", "zen"
+];
